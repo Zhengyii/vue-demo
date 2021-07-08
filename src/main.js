76dac0a04b63f8b751ee5fe63a7aae32d6d3e340
ZhengYi <zhengyi@induschain.cn>
2021-07-08 16:28:26 +0800

    feat: 🐧TodoItem组件

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 // 项目核心文件：进行全局配置
-import Vue from 'vue'
-import App from './App.vue'
-// 引入Element-Ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css'; // 样式文件需单独引入
+import Vue from "vue";
+import App from "./App.vue";
+// // 引入Element-Ui
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css'; // 样式文件需单独引入
 
-// 
-Vue.config.productionTip = false
+//
+Vue.config.productionTip = false;
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 
 // 创建一个实例
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
