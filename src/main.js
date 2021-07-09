c4faecd352e10f35912ec379207a4deb6d0b744d
ZhengYi <zhengyi@induschain.cn>
2021-07-09 15:44:15 +0800

    feat: 🐧Vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // 项目核心文件：进行全局配置
 import Vue from "vue";
 import App from "./App.vue";
+import router from './router'
 // // 引入Element-Ui
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css'; // 样式文件需单独引入
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 
 // 创建一个实例
 new Vue({
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount("#app");
